b5e49cd4ff10f7320bfe2291a4eda87af0264fa7
Ofelia888 <acbk123@gmail.com>
2025-08-21 10:16:35 +0200

    Refactored pluklist.cs

--- Plukliste/Plukliste.cs
+++ Plukliste/Plukliste.cs
@@ -1,24 +1,45 @@
 ﻿namespace Plukliste;
+
+// The pluklist class containing a list, identifier properties and a method for adding items to the list
 public class Pluklist
 {
-    public string? Name;
-    public string? Forsendelse;
-    public string? Adresse;
-    public List<Item> Lines = new List<Item>();
-    public void AddItem(Item item) { Lines.Add(item); }
+    public string Name { get; set; }
+    public string Shipment { get; set; }
+    public string Address { get; set; }
+    public List<Item> Lines { get; set; }
+    public Pluklist(string name, string shipment, string address)
+    {
+        Name = name;
+        Shipment = shipment;
+        Address = address;
+        Lines = new List<Item>();
+    }
+    public void AddItem(Item item)
+    {
+        Lines.Add(item);
+    }
 }
 
+// The item class containing identifier properties
 public class Item
 {
-    public string ProductID;
-    public string Title;
-    public ItemType Type;
-    public int Amount;
+    public string ProductID { get; set; }
+    public string Title { get; set; }
+    public ItemType Type { get; set; }
+    public int Amount { get; set; }
+    public Item(string productID, string title, ItemType type, int amount)
+    {
+        ProductID = productID;
+        Title = title;
+        Type = type;
+        Amount = amount;
+    }
 }
 
 public enum ItemType
 {
-    Fysisk, Print
+    Fysisk,
+    Print
 }
 
 
