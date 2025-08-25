cdd0dcf341eb5ad65bfbb12be7ac9ab3d46d2192
Arikuroshi <71873@edu.sde.dk>
2025-08-25 09:19:13 +0200

    OptionPrinter added as it's own class

--- Case/ConsolePrinter.cs
+++ Case/ConsolePrinter.cs
@@ -12,22 +12,6 @@ namespace PluckList
         public abstract void Print(string text);
     }
 
-    public class OptionPrinter : ConsolePrinter
-    {
-        public ColorHandle ColorHandle { get; set; }
-        public OptionPrinter()
-        {
-            ColorHandle = new ColorHandle();
-        }
-        public override void Print(string text)
-        {
-            ColorHandle.Handle(ColorContext.Option);
-            Console.Write(text.First());
-            ColorHandle.Handle(ColorContext.Standard);
-            Console.WriteLine(text.Remove(0, 1));
-        }
-    }
-
     //public class FileInfoPrinter : ConsolePrinter
     //{
     //    public FileReader FileReader { get; set; }
