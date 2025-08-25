359a10e44e45c233c793ee8953a985fe090e2796
Karim <72211@edu.sde.dk>
2025-08-25 10:56:53 +0200

    Add HTMLTemplate class

---
+++ Case/HTMLTemplate.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PluckList;
+
+public partial class HTMLTemplate
+{
+    private readonly string _contents;
+    private readonly IEnumerable<string> _variables;
+    
+    private HTMLTemplate(string contents)
+    {
+        _contents = contents;
+        var matches = VariablePattern().Matches(contents);
+        _variables = matches.Select(match => match.Groups[1].Value);
+    }
+
+    public IEnumerable<string> GetVariables()
+    {
+        return _variables;
+    }
+
+    public string GetContents()
+    {
+        return _contents;
+    }
+    
+    public string GetContents(Dictionary<string, string> variables)
+    {
+        var commonVariables = _variables.Where(variables.ContainsKey);
+        return commonVariables.Aggregate(_contents,
+            (contents, variable) => contents.Replace($"[{variable}]", variables[variable]));
+    }
+
+    public static HTMLTemplate? Load(string path)
+    {
+        if (!File.Exists(path)) return null;
+        var contents = File.ReadAllText(path);
+        var template = new HTMLTemplate(contents);
+        return template;
+    }
+
+    [GeneratedRegex(@"\[(.*?)\]")]
+    private static partial Regex VariablePattern();
+}
