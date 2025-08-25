e75d0118e9cc44b92cd81f415e286f9d29ee7991
Karim <72211@edu.sde.dk>
2025-08-25 11:23:49 +0200

    Create HTML file from template

--- Case/FileMover.cs
+++ Case/FileMover.cs
@@ -1,4 +1,6 @@
-﻿namespace PluckList
+﻿using System.Xml.Serialization;
+
+namespace PluckList
 {
     public class FileMover
     {
@@ -11,9 +13,25 @@
         
         public void Move(int index)
         {
+            Directory.CreateDirectory("print");
+            var htmlName = Path.Combine("print", $"{Directory.GetFiles("print").Length + 1}.html");
+            var html = Path.Combine("templates", "PRINT-WELCOME.html");
+            
+            var fileStream = File.OpenRead(_files[index]);
+            var xmlSerializer = new XmlSerializer(typeof(PluckList));
+            var pluckList = (PluckList?)xmlSerializer.Deserialize(fileStream);
+            fileStream.Close();
+
+            var vars = new Dictionary<string, string>();
+            vars.Add("Name", pluckList.Name);
+            vars.Add("Adresse", pluckList.Address);
+            vars.Add("Plukliste",
+                string.Join($"<br>{Environment.NewLine}", pluckList.Lines.Select(item => $"{item.Title} (x{item.Amount})")));
+            File.WriteAllText(htmlName, HTMLTemplate.Load(html)?.GetContents(vars) ?? string.Empty);
+            
             Directory.CreateDirectory("import");
 
-            var fileName = Path.GetFileName(_files[0]);
+            var fileName = Path.GetFileName(_files[index]);
             File.Move(_files[index], string.Format(@"import\\{0}", fileName), true);
 
             Console.WriteLine($"Plukseddel {_files[index]} afsluttet.");
